289952bc92f43245324a52e92263331df4c916a7
jaehwanspin <jaehwanspin@gmail.com>
2020-03-16 19:26:27 +0900

    asdffdsa

--- include/awesome_async_fx/active_object/active_object.hpp
+++ include/awesome_async_fx/active_object/active_object.hpp
@@ -1,7 +1,6 @@
 #ifndef _AASYNC_ACTIVE_OBJECT_HPP_
 #define _AASYNC_ACTIVE_OBJECT_HPP_
 #include "../aasync_namespace.hpp"
-#include "./base/active_object_base.hpp"
 
 namespace _AASYNC_NAMESPACE_NAME
 {
