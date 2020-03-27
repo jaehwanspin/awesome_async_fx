67836f43c0d7d4ac2ace63fb2933457da9eb9b01
elcomtech <jaehwanspin@gmail.com>
2020-03-27 18:44:23 +0900

    asdfasdf

---
+++ include/awesome_async_fx/active_object/timer.hpp
@@ -0,0 +1,57 @@
+#ifndef _AASYNC_TIMER_HPP_
+#define _AASYNC_TIMER_HPP_
+#include "../aasync_namespace.hpp"
+
+#include <ctime>
+#include <chrono>
+
+namespace _AASYNC_NAMESPACE_NAME
+{
+
+class timer
+{
+public:
+    void start()
+    {
+        m_StartTime = std::chrono::system_clock::now();
+        m_bRunning = true;
+    }
+    
+    void stop()
+    {
+        m_EndTime = std::chrono::system_clock::now();
+        m_bRunning = false;
+    }
+    
+    double elapsedMilliseconds()
+    {
+        std::chrono::time_point<std::chrono::system_clock> endTime;
+        
+        if(m_bRunning)
+        {
+            endTime = std::chrono::system_clock::now();
+        }
+        else
+        {
+            endTime = m_EndTime;
+        }
+        
+        return std::chrono::duration_cast<std::chrono::milliseconds>(
+            end_time - start_time
+        ).count();
+    }
+    
+    double elapsedSeconds()
+    {
+        return elapsedMilliseconds() / 1000.0;
+    }
+
+private:
+    std::chrono::time_point<std::chrono::system_clock> start_time;
+    std::chrono::time_point<std::chrono::system_clock> end_time;
+    bool is_running = false;
+};
+
+}
+
+#endif
