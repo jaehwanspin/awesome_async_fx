5c42303a9c11ef1e905123e0bfde75f3af0c6d18
elcomtech <jaehwanspin@gmail.com>
2020-04-02 18:08:10 +0900

    fdsaasdf

--- src/active_object/active_object.cc
+++ src/active_object/active_object.cc
@@ -1,2 +1,22 @@
 #include <awesome_async_fx/active_object/active_object.hpp>
 
+namespace _AASYNC_NAMESPACE_NAME {
+
+active_object::active_object()
+{
+    
+}
+
+active_object::~active_object()
+{
+    if (this->handler.joinable())
+        this->handler.join();
+}
+
+
+void active_object::open()
+{
+    this->handler = std::thread();
+}
+
+}
