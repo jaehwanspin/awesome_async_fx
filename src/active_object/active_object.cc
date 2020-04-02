17085b0e58bf0ec859de4d4f452f67741df0b6e2
elcomtech <jaehwanspin@gmail.com>
2020-04-02 18:55:45 +0900

    fdsaasdf

--- src/active_object/active_object.cc
+++ src/active_object/active_object.cc
@@ -4,19 +4,27 @@ namespace _AASYNC_NAMESPACE_NAME {
 
 active_object::active_object()
 {
-    
+    active_object::obj_list.push_back(std::shared_ptr<active_object>(this));
 }
 
 active_object::~active_object()
 {
-    if (this->handler.joinable())
-        this->handler.join();
+    if (this->object_handler.joinable())
+        this->object_handler.join();
 }
 
 
 void active_object::open()
 {
-    this->handler = std::thread();
+    this->object_handler = std::thread(this->run);
+}
+
+void active_object::run()
+{
+    while (!this->exit_flag)
+    {
+
+    }
 }
 
 }
